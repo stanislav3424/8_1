e23d6a813a167501f2cf9746ea26cd9e2a30b682
test <doudoladov_stanislav@mail.ru>
2025-03-23 10:38:43 +0300

    Add project files.

---
+++ 8_1/8_1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+class bad_length : public std::exception
+{
+};
+
+int function(std::string str, int forbidden_length)
+{
+    int length = str.length();
+    if (length == forbidden_length)
+    {
+        throw bad_length();
+    }
+    return length;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    std::cout << "Введите запретную длину: ";
+    int forbidden_length{};
+    std::cin >> forbidden_length;
+
+    while (true)
+    {
+        try
+        {
+            std::string input{};
+            std::cout << "Введите слово: ";
+            std::cin >> input;
+            std::cout << "Длина слова \"" << input << "\" равна " << function(input, forbidden_length) << std::endl;
+        }
+        catch (bad_length)
+        {
+            std::cout << "Вы ввели слово запретной длины! До свидания" << std::endl;
+            return 1;
+        }
+    }
+
+    return 0;
+}
